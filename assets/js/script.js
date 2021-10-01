fa52a41a0a1c745c07a31769202337d2965b9d9e
Matthew Palmer <mattjpalmer92@gmail.com>
2021-10-01 18:09:27 +0100

    completed homework with help from Suraj

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,197 @@
 // Assignment code
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-// G L O B A L  A R R A Y S 
+// G L O B A L  A R R A Y S
 
 // arrays containing characters, symbols & numbers used in password generation
 
 // symbols
-const symbolCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+const symbolCharacter = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  " < ",
+  "=",
+  " > ",
+  " ? ",
+  "@",
+  "[",
+  "\\",
+  "]",
+  " ^ ",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 // numbers
 const numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // lowercase
-const lowerCaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowerCaseCharacter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 // uppercase
-const upperCaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const upperCaseCharacter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-// V A R I A B L E S 
-
-// password option variables
-
-const selectSymbolCharacter;
-const selectNumberCharacter;
-const selectLowerCaseCharacter;
-const selectUpperCaseCharacter;
-
-
-// F U N C T I O N 
-
-// create password function 
+// F U N C T I O N
+// const createPassword = function () {}
+// create password function
 function createPassword() {
-    // request input
+  // request input
+  const passwordLength = parseInt(
+    prompt("Please input a password length between 8 and 128")
+  );
+  // validate input (between 8-128)
+  if (passwordLength < 8 || passwordLength > 128) {
+    // if password is invalid length
+    alert(
+      "Password length must be between 8-128 characters in length, please try again"
+    );
+    return "";
+  }
 
-    // validate input (between 8-128)
+  // password option variables
+  const selectSymbolCharacters = confirm("Do you need symbol characters?");
+  const selectNumberCharacters = confirm("Do you need numbers?");
+  const selectLowerCaseCharacters = confirm(
+    "Do you need lowercase characters?"
+  );
+  const selectUpperCaseCharacters = confirm(
+    "Do you need uppercase characters?"
+  );
 
-    // if invalid
+  // check if user has selected at least one option
+  if (
+    !selectSymbolCharacters &&
+    !selectNumberCharacters &&
+    !selectLowerCaseCharacters &&
+    !selectUpperCaseCharacters
+  ) {
+    alert("You must select atleast one option");
+    return "";
+  }
 
-    // if valid continues to criteria request
-        // confirm symbol
+  // array with selected choices
+  const chosen = [];
 
-        // confirm number
+  // if valid continues to criteria request
+  // confirm symbol
+  if (selectSymbolCharacters) {
+    chosen.push(symbolCharacter);
+  }
+  // confirm number
+  if (selectNumberCharacters) {
+    chosen.push(numberCharacter);
+  }
+  // confirm lower case
+  if (selectLowerCaseCharacters) {
+    chosen.push(lowerCaseCharacter);
+  }
+  // confirm upper case
+  if (selectUpperCaseCharacters) {
+    chosen.push(upperCaseCharacter);
+  }
 
-        // confirm lower case
+  // array for generated password
+  const password = [];
 
-        // confirm upper case
+  for (let i = 0; i < passwordLength; i++) {
+    // get random index from chosen array
+    const randomChosenArrayIndex = Math.floor(Math.random() * chosen.length);
 
-    // if four negative values inputted
-    
-    // if values inputted, stored here
-    const passwordCriteria = []
-    // if symbol selected
+    // get random character set
+    const randomCharacterSet = chosen[randomChosenArrayIndex];
 
-    // if number selected
+    // get random character index
+    const randomCharacterIndex = Math.floor(
+      Math.random() * randomCharacterSet.length
+    );
 
-    // if lower case selected
+    // get random character
+    const randomCharacter = randomCharacterSet[randomCharacterIndex];
 
-    // if upper case selected
+    password.push(randomCharacter);
+  }
 
-    // password string composed of random characters from chosen criterias in passwordCriteria array
-
+  // convert array for generated password to string
+  return password.join("");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// LEAVE CODE BELOW ALONE 
+// LEAVE CODE BELOW ALONE
 
 // Write password to the #password input
 function writePassword() {
-    const password = generatePassword();
-    const passwordText = document.querySelector("#password");
+  var password = createPassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-
+  passwordText.value = password;
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
-
+generateBtn.addEventListener("click", writePassword);
